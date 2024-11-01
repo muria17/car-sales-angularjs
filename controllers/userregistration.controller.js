b7fd0b4e79647ae07b1b8234bf46718b8ee97797
Muria <financas.antonio@gmail.com>
2024-11-01 09:12:17 -0300

    criando novos usuarios

--- controllers/userregistration.controller.js
+++ controllers/userregistration.controller.js
@@ -1,25 +1,27 @@
 angular.module('myApp') 
 .controller('UserRegistrationController', ['$scope', '$location','$rootScope','userregistration',  function($scope, $location, $rootScope, userRegistration) {
 
-    $scope.Data = {};  
+    $scope.data = {};  
     $scope.errorMessage = '';
 
     $scope.validateData = function() {
-        if ($scope.Data.login && $scope.Data.userAccountPassword) {
+        if ($scope.data.login && $scope.data.userAccountPassword && $scope.data.customerId) {
             $scope.createNewUser();
         } else {
-            $scope.errorMessage = '*Os campos de Username e Password não podem ficar em branco.';
+            $scope.errorMessage = '*Os campos de Username, Password e Customer não podem ficar em branco.';
         }
     };
 
     $scope.createNewUser = function() {
         $scope.loading = true; 
         $rootScope.isLoggedIn = true; 
-        userRegistration.createNewUser($scope.loginData).then(function(response) {
+        userRegistration.createNewUser($scope.data).then(function(response) {
             if (response.status === 200) {
                 $rootScope.isLoggedIn = true; 
                 $scope.userAccount = response.data; 
                 $scope.loading = false; 
+                $scope.successMessage = "Cadastro realizado com sucesso!";
+                $location.path('/login'); 
             } else {
                 $scope.errorMessage = "Tivemos um problema ao cadastrar o seu usuario, tente novamente mais tarde.";
                 $scope.loading = false; 
