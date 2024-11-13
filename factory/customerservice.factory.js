e7ad6534f60a84ac68c1ec68f960c795c1425db2
Muria <financas.antonio@gmail.com>
2024-11-13 09:04:29 -0300

    criando e deletando customers

--- factory/customerservice.factory.js
+++ factory/customerservice.factory.js
@@ -3,7 +3,28 @@ angular.module('myApp')
     var service = {};
 
     service.getCustomers = function() {
-        return $http.get('https://carsales-production.up.railway.app/client' ); 
+        return $http.get('https://carsales-production.up.railway.app/client' );     };
+
+    service.saveCustomer = function(newCustomer) {
+        return $http.post('https://carsales-production.up.railway.app/client' , newCustomer)
+            .then(function(response) {
+                console.log("Dados enviados para validação:", newCustomer);
+                return response;  
+            }, function(error) {
+                console.error('Erro na requisição ao backend:', error);
+                return error;  
+            });
+    };
+
+    service.deleteCustomer = function(customerId) {
+        return $http.delete('https://carsales-production.up.railway.app/client/' + customerId)
+            .then(function(response) {
+                console.log("Dados enviados para validação:", response);
+                return response;  
+            }, function(error) {
+                console.error('Erro na requisição ao backend:', error);
+                return error;  
+            });
     };
 
     return service;
